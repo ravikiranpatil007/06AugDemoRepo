c399c417bac3ab762224e73c61c6d502c1c739f2
ravikiranpatil007 <122557467+ravikiranpatil007@users.noreply.github.com>
2023-01-17 22:35:51 +0530

    Bask Task
    Hi,
    I have completed basic task.
    
    Thanks,
    Ravikiran

---
+++ GitHub_SampleProject/src/mypacakage/TestBase.java
@@ -0,0 +1,11 @@
+package mypacakage;
+
+public class TestBase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello GitHub Ready To Launch");
+	}
+
+}
